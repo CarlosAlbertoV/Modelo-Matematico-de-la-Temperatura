7358364833c1abde84dd05cd234e623984c6b818
Irving Ventura <39660540+CarlosAlbertoV@users.noreply.github.com>
2021-06-23 23:34:15 -0500

    Add files via upload

---
+++ ModeloMatematicoDeLaTemperatura_IrvingDanielVenturaHernández_3202.m
@@ -0,0 +1,21 @@
+% Título: Modelo matemático de temperatura
+% Descripción: Scrip para graficar y sacar dominio y rango.
+% Autor: Irving Daniel Ventura Hernández.
+% Fecha: 15 de abril de 2021.
+
+%C(f) = ((f*5)/9)+32
+
+%limpiar variables
+clear
+%rango de -212 .. 212 en i = 0.2
+f=-212:0.2:212;
+% valor de la funcion
+C= ((f*5)/9)+32;
+%dibujar x,y 
+plot (f,C);
+%titulo
+title("C(f) =((f*5)/9)+32");
+% etiqueta para x
+xlabel ("Fahrenheit");
+%etiqueta para y
+ylabel ("C(x)");
